1c63c464024bd12e2386a1cfb5db2f67f5f8d405
Vincent Hagen <vinnie@script4web.nl>
2022-04-12 20:48:36 +0200

    add link to english to footer

--- src/components/Layout/Main/Footer/Footer.tsx
+++ src/components/Layout/Main/Footer/Footer.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { FooterContainer, FooterWrapper } from './components';
+import { FooterContainer, FooterFlagImg, FooterWrapper } from './components';
 import Socials from './Socials/Socials';
 import TrustPilot from './Trustpilot/Trustpilot';
+import ukFlag from '../../../../images/uk-flag.png';
+import { Link } from 'gatsby';
 
 const FooterText: React.FunctionComponent = () => {
     return (
         <div>
             © 2021 Vereniging Anderwijs. KvK: 40480422. Contact:
-            <a href="mailto:info@anderwijs.nl"> info@anderwijs.nl</a>.
+            <a href="mailto:info@anderwijs.nl"> info@anderwijs.nl</a>.<br />
+            <FooterFlagImg src={ukFlag} alt="UK flag icon"></FooterFlagImg>
+            Looking for information in English?{' '}
+            <Link to="/contact/information-in-english">Click here!</Link>
         </div>
     );
 };
