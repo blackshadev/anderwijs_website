ffd184012f183fe9ad7c41e35d08b15584cfbd1c
Vincent Hagen <vinnie@script4web.nl>
2022-09-26 17:09:19 +0200

    Fix 'kamp-agenda' as it is only rendered once

--- src/components/WysiwygContent/ContentReplacers/AddUpcomingEvents.tsx
+++ src/components/WysiwygContent/ContentReplacers/AddUpcomingEvents.tsx
@@ -1,23 +1,22 @@
+import { DOMNode, Element } from 'html-react-parser';
 import React, { ReactElement } from 'react';
 import UpcomingEvents from '../../UpcomingEvents';
 import { Replacer } from './Replacer';
 
 export default class AddUpcomingEvents implements Replacer {
-    private isFirst = true;
     public acceptsRoute(location: Location): boolean {
         return location.pathname === '/onze-bijleskampen/kampagenda/';
     }
 
-    public supportsNode(): boolean {
-        return true;
+    public supportsNode(node: DOMNode): boolean {
+        return (
+            node instanceof Element &&
+            node.tagName === 'span' &&
+            node.attribs['data-replacer'] === 'upcoming-events'
+        );
     }
 
     public getNode(): ReactElement {
-        if (!this.isFirst) {
-            return <></>;
-        }
-
-        this.isFirst = false;
         return <UpcomingEvents />;
     }
 }
